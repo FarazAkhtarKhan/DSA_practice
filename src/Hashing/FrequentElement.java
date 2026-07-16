ab55597bb486832faba96bf9af23285e7f4742e3
FarazAkhtarKhan <faraz.akhtar.khan112@gmail.com>
2026-07-16 23:08:07 +0530

    used hashmap

--- src/Hashing/FrequentElement.java
+++ src/Hashing/FrequentElement.java
@@ -1,25 +1,40 @@
 package Hashing;
-
+import java.util.*;
 public class FrequentElement {
     public int mostFrequentElement(int[] nums) {
-        int maxElement = nums[0];
-        for(int num : nums){
-            if(num > maxElement) maxElement = num;
-        }
-        int hash[] = new int[maxElement+1];
-        for(int i = 0; i < nums.length;i++){
-            hash[nums[i]]+=1;
-        }
+//        int maxElement = nums[0];
+//        for(int num : nums){
+//            if(num > maxElement) maxElement = num;
+//        }
+//        int hash[] = new int[maxElement+1];
+//        for(int i = 0; i < nums.length;i++){
+//            hash[nums[i]]+=1;
+//        }
+//
+//        int maxFrequency = hash[0];
+//        int count = 0;
+//        for(int i = 0; i < hash.length;i++){
+//            if (hash[i] > maxFrequency) {
+//                maxFrequency = hash[i];
+//                count = i;
+//            }
+//        }
+//        return count;
 
-        int maxFrequency = hash[0];
-        int count = 0;
-        for(int i = 0; i < hash.length;i++){
-            if (hash[i] > maxFrequency) {
-                maxFrequency = hash[i];
-                count = i;
+
+        //Using Hashmap
+
+        HashMap<Integer,Integer> ele_freq = new HashMap<Integer, Integer>();
+        int maxCount = 0;
+
+        for(int num : nums){
+            //this check if number exists if not put it to zero, if its there increment the value to 1
+            int currentCount = ele_freq.getOrDefault(num,0) + 1;
+            ele_freq.put(num, currentCount);
+            if(currentCount > maxCount){
+                maxCount = currentCount;
             }
         }
-        return count;
-
+        return maxCount;
     }
 }
